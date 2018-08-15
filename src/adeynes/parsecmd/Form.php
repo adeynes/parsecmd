a88632bc7a948ff6349c2bfca4c8f7dce9f7de42
adeynes <alexisdeynes@outlook.com>
2018-08-15 16:02:31 +0200

    More form methods

--- src/adeynes/parsecmd/Form.php
+++ src/adeynes/parsecmd/Form.php
@@ -21,7 +21,7 @@ class Form
     /**
      * @var array Nice human-readable names to reference fields instead of good ol' numbers
      */
-    protected $labels = [];
+    protected $aliases = [];
 
     public function __construct(string $id)
     {
@@ -51,27 +51,72 @@ class Form
         $this->data['title'] = $title;
     }
 
-    public function addContent(array $content): self
+    protected function addContent(array $content, ?string $alias): self
     {
         $this->data['content'][] = $content;
+        $this->addAlias($alias);
         return $this;
     }
 
-    public function addLabel(?string $label): void
+    public function addAlias(?string $alias): void
     {
-        $this->labels[] = $label ?? count($this->labels);
+        $this->aliases[] = $alias ?? count($this->aliases);
     }
 
-    public function addToggle(string $text, bool $default = false, string $label = null): self
+    public function addLabel(string $text, string $alias = null): self
     {
-        $this->addLabel($label);
-        return $this->addContent(['type' => 'toggle', 'text' => $text, 'default' => $default]);
+        return $this->addContent(
+            ['type' => 'label', 'text' => $text],
+            $alias
+        );
     }
 
-    public function addInput(string $text, string $placeholder = '', string $default = null, string $label = null): self
+    public function addInput(string $text, string $placeholder = '', string $default = '', string $alias = null): self
     {
-        $this->addLabel($label);
-        return $this->addContent(['type' => 'input', 'text' => $text, 'placeholder' => $placeholder, 'default' => $default]);
+        return $this->addContent(
+            ['type' => 'input', 'text' => $text, 'placeholder' => $placeholder, 'default' => $default],
+            $alias
+        );
+    }
+
+    public function addToggle(string $text, bool $default = false, string $alias = null): self
+    {
+        return $this->addContent(
+            ['type' => 'toggle', 'text' => $text, 'default' => $default],
+            $alias
+        );
+    }
+
+    public function addSlider(string $text, int $min, int $max, int $step = null, int $default = null, string $alias = null): self
+    {
+        $content = ['type' => 'slider', 'text' => $text, 'min' => $min, 'max' => $max];
+        if (!is_null($step)) {
+            $content['step'] = $step;
+        }
+        if (!is_null($default)) {
+            $content['default'] = $default;
+        }
+        return $this->addContent($content, $alias);
+    }
+    
+    public function addStepSlider(string $text, array $steps, int $default = null, string $alias = null): self
+    {
+        $content = ['type' => 'step_slider', 'text' => $text, 'steps' => $steps];
+        if (!is_null($default)) {
+            $content['default'] = $default;
+        }
+
+        return $this->addContent($content, $alias);
+    }
+
+    public function addDropdown(string $text, array $options, int $default = null, string $alias = null): self
+    {
+        $content = ['type' => 'dropdown', 'text' => $text, 'options' => $options];
+        if (!is_null($default)) {
+            $content['default'] = $default;
+        }
+
+        return $this->addContent($content, $alias);
     }
 
     public function send(Player $player): void
